382fa778b9ec61b53d69f0cc7bc7a125a75bfa8b
Frank Mckenna <fmckenna56330@gmail.com>
2022-01-15 10:45:07 +0000

    chore: add comments to searchBars component

--- src/Components/SearchBars/SearchBars.js
+++ src/Components/SearchBars/SearchBars.js
@@ -6,29 +6,37 @@ import './SearchBars.css'
 function SearchBars({ setStudents, students }) {
   const studentsRef = useRef(students);
   const tagState = useRecoilValue(tagsState);
-  const [filter, setFilter] = useState(['', ''])
+  const [preferences, setPreferences] = useState(['', ''])
 
-  const filterPreferences = (value) => {
-    setFilter(value)
+  const filterPreferences = (nameAndTag) => {
+    const allStudents = studentsRef.current[0];
+    setPreferences(nameAndTag);
+
+    // save a reference of all students if none
     studentsRef.current.length < 1 && studentsRef.current.push(students);
 
-    const val = (str) => str?.toUpperCase().startsWith(value[0]?.toUpperCase());
+    // Search if students full name starts with the name typed
+    const searchByName = (str) => str?.toUpperCase().startsWith(nameAndTag[0]?.toUpperCase());
 
-    const filterStudents = studentsRef.current[0].filter(student => {  
-      let { firstName } = student;
-      let tagFound = tagState.find(tag => tag[firstName]?.startsWith(value[1]));
-      let foundStudent = val(student.firstName) || val(student.lastName);
+    // filter by name and tag
+    const filterStudents = allStudents.filter(student => {  
+      let { firstName, lastName } = student;
+      
+      // returns true if a student is found with the tag typed
+      let tagFound = tagState.find(tag => tag[firstName]?.startsWith(nameAndTag[1]));
+      let foundStudent = searchByName(firstName) || searchByName(lastName);
 
-      if (value[0] && value[1]) {
+      if (nameAndTag[0] && nameAndTag[1]) {
         return tagFound && foundStudent ? true : false;
-      } else if (value[0] && !value[1]) {
+      } else if (nameAndTag[0] && !nameAndTag[1]) {
         return foundStudent ? true : false;
-      } else if (value[1] && !value[0]) {
+      } else if (nameAndTag[1] && !nameAndTag[0]) {
         return tagFound;
       }
     });
 
-    value[0] || value[1] ? setStudents([...filterStudents]) : setStudents(studentsRef.current[0]);
+    // if there is a value in any input field then set the filtered students otherwise set all student from the reference
+    nameAndTag[0] || nameAndTag[1] ? setStudents([...filterStudents]) : setStudents(allStudents);
   }
 
   return (
@@ -38,8 +46,8 @@ function SearchBars({ setStudents, students }) {
           data-testid='input-name'
           className="name-input"
           type='text'
-          value={filter[0] || ''}
-          onChange={(e) => filterPreferences([e.target.value, filter[1]])}
+          value={preferences[0] || ''}
+          onChange={(e) => filterPreferences([e.target.value, preferences[1]])}
           placeholder='Search by name'
         />
       </div>
@@ -48,10 +56,10 @@ function SearchBars({ setStudents, students }) {
         <input
           data-testid='input-tag'
           className="tag-input"
-          value={filter[1] || ''}
+          value={preferences[1] || ''}
           type='text'  
           placeholder='Search by tag'
-          onChange={(e) => filterPreferences([filter[0], e.target.value])}
+          onChange={(e) => filterPreferences([preferences[0], e.target.value])}
         />
       </div>
     </div>
